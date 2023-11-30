264160760adcc3d4481f69e2a163885b075383c0
bcoll <bcoll@cloudflare.com>
2023-11-30 21:40:33 +0000

    Use custom test runner and reset fetch mock at the start of each file

--- packages/vitest-pool-workers/test/kv/store.test.ts
+++ packages/vitest-pool-workers/test/kv/store.test.ts
@@ -5,15 +5,17 @@ import {
 	runWithDurableObjectContext,
 	runDurableObjectAlarm,
 } from "cloudflare:test";
-import { describe, it, expect, afterAll } from "vitest";
+import { describe, it, expect, afterAll, beforeAll } from "vitest";
 import worker, { transformResponse, Counter } from "./worker";
 
-fetchMock.activate();
-fetchMock.disableNetConnect();
-fetchMock
-	.get("https://example.com")
-	.intercept({ path: "/" })
-	.reply(200, "data");
+beforeAll(() => {
+	fetchMock.activate();
+	fetchMock.disableNetConnect();
+	fetchMock
+		.get("https://example.com")
+		.intercept({ path: "/" })
+		.reply(200, "data");
+});
 afterAll(() => fetchMock.assertNoPendingInterceptors());
 
 describe("kv", () => {
